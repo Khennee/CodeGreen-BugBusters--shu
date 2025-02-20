0ae83784fec3a4c78394b67f9367dc6e45745782
Khennee <iambishawn@gmail.com>
2025-02-20 22:05:12 +0800

    Update Login.stories.tsx

--- frontend/src/stories/Login.stories.tsx
+++ frontend/src/stories/Login.stories.tsx
@@ -2,7 +2,7 @@ import { expect } from "@storybook/test";
 import { Meta } from "@storybook/react";
 import LoginPage from "../pages/LoginPage";
 import { BrowserRouter } from "react-router-dom";
-import { userEvent, within, screen, waitFor } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 
 export default {
   title: "Pages/LoginPage",
@@ -27,38 +27,26 @@ DefaultState.play = async ({ canvasElement }: { canvasElement: HTMLElement }) =>
   await expect(passwordInput).toHaveValue('');
 };
 
-export const SuccessState = () => <LoginPage />;
 
-SuccessState.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+// there will be a pop up of "Something went wrong, Please Try Again." because of the line 26-27
+// on the LoginPage.tsx because it doesnt navigate to the homepage.
+// for further explanation please see lines 21-29 in LoginPage.tsx
+export const WithPassword = () => <LoginPage />;
+WithPassword.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
   const canvas = within(canvasElement);
   const emailInput = canvas.getByPlaceholderText("Email address");
   const passwordInput = canvas.getByPlaceholderText("Enter your password");
-  const loginButton = canvas.getByTestId("login-button");
 
-  await userEvent.type(emailInput, 'testforsuccess@gmail.com');
-  await userEvent.type(passwordInput, 'success');
-  await userEvent.click(loginButton);
-
-  await waitFor(() => {
-    expect(screen.getByText(/login successful/i)).toBeInTheDocument();
-  });
-
-  await expect(loginButton).toBeEnabled();
+  await userEvent.type(emailInput, 'withpassword@gmail.com');
+  await userEvent.type(passwordInput, 'valid');
+  await userEvent.keyboard("{Enter}")
 };
 
-export const FailState = () => <LoginPage />;
-
-FailState.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+export const MissingPassword = () => <LoginPage />;
+MissingPassword.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
   const canvas = within(canvasElement);
   const emailInput = canvas.getByPlaceholderText("Email address");
-  const passwordInput = canvas.getByPlaceholderText("Enter your password");
-  const loginButton = canvas.getByTestId("login-button");
 
-  await userEvent.type(emailInput, 'testforfailgmail.com');
-  await userEvent.type(passwordInput, 'thereisno@sign');
-  await userEvent.click(loginButton);
-  await expect(
-    await canvas.findByText("Something went wrong, Please Try Again."),
-  ).toBeInTheDocument();
-
+  await userEvent.type(emailInput, 'nopassword@gmail.com');
+  await userEvent.keyboard("{Enter}")
 };
